89f32c4c60cf5a4405f284ff9bafff63c56b54e0
haqnawaz <haqnawaz99@yahoo.com>
2023-01-16 21:11:47 +0500

    Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
 import { useState } from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Flex , Stack, VStack, HStack} from '@chakra-ui/react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      {/* the first box will be div second will be section
      <Box bg='green' w='120px' h='150px'>
       This is a box
       </Box>
 
       <Box as='section' bg='green' w='120px' h='150px'>
       This is a box
+      </Box> */}
+      {/* //Flex// */}
+      {/* <Flex justify="space-between" align="center">
+      <Box bg='green' w='100px' h='200px'>
+        item1
       </Box>
+      <Box bg='blue' w='100px' h='200px'>
+        item2
+      </Box>
+      <Box bg='red' w='200px' h='100px'>
+        item1
+      </Box>
+      <Box bg='tomato' w='100px' h='200px'>
+        item4
+      </Box>
+      </Flex> */}
+      {/* //Stack
+<Stack spacing='12px' direction='row'>
+      <Box bg='green' w='100px' h='200px'>
+        item1
+      </Box>
+      <Box bg='blue' w='100px' h='200px'>
+        item2
+      </Box>
+      <Box bg='red' w='200px' h='100px'>
+        item1
+      </Box>
+      <Box bg='tomato' w='100px' h='200px'>
+        item4
+      </Box>
+      </Stack> */}
+      <Header/>
     </div>
   )
 }
